0bd38e9cd49d08e319b16a60491aaba88cd33795
Pedro Almeida <pedroalmeida.9188@gmail.com>
2025-06-24 22:43:09 -0300

    Add counter algorithm to count up to input number

---
+++ python-exercises/Contagem.py
@@ -0,0 +1,15 @@
+#testando o for na prática - contagem até 10
+for i1 in range(11):
+    print(i1)
+print("----------------------------------")
+#testando for na prática - contagem a partir do número 3 até 10
+for i2 in range(3, 11):
+    print(i2)
+print("----------------------------------")
+#testando for na prática - contagem a partir do número 2 até 20, pulando 2 em 2, apenas números pares
+for i3 in range(2, 20, 2):
+    print(i3)
+print("----------------------------------")
+#testando for na prática - contagem a partir do número 1 até 20, pulando 2 em 2, apenas números impares
+for i4 in range(1, 20, 2):
+    print(i4)
